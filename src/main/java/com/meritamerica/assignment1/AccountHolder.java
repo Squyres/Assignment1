afa95dadbc0728edf6391d63f16e891319f0b0bc
Jameson <jamesonsquyres@gmail.com>
2020-10-03 16:27:29 -0500

    Initial files

--- src/main/java/com/meritamerica/assignment1/AccountHolder.java
+++ src/main/java/com/meritamerica/assignment1/AccountHolder.java
@@ -2,4 +2,41 @@ package com.meritamerica.assignment1;
 
 public class AccountHolder {
 	
+	AccountHolder() {
+	}
+	AccountHolder(String firstName, String middleName, String lastName, String ssn, double checkingAccountOpeningBalance, double savingsAccountOpeningBalance) {
+	}
+	String getFirstName() {
+		
+	}
+	void setFirstName(String firstName) {
+		
+	}
+	String getMiddleName() {
+		
+	}
+	void setMiddleName(String firstName) {
+		
+	}
+	String getLastName() {
+		
+	}
+	void setLastName(String firstName) {
+		
+	}
+	String getSSN() {
+		
+	}
+	void setSSN(String SSN) {
+		
+	}
+	CheckingAccount getCheckingAccount() {
+		
+	}
+	SavingsAccount getSavingsAccount() {
+		
+	}
+	String toString() {
+		
+	}
 }
--- src/main/java/com/meritamerica/assignment1/CheckingAccount.java
+++ src/main/java/com/meritamerica/assignment1/CheckingAccount.java
@@ -2,4 +2,25 @@ package com.meritamerica.assignment1;
 
 public class CheckingAccount {
 	
+	CheckingAccount(double openingBalance) {
+		
+	}
+	double getBalance() {
+		
+	}
+	double getInterestRate() {
+		
+	}
+	boolean withdraw(double amount) {
+		
+	}
+	boolean deposit(double amount) {
+		
+	}
+	double futureValue(int years) {
+		
+	}
+	String toString() {
+		
+	}
 }
